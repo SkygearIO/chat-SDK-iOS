8be02391e0962f95ba5027c870337dd6a7646741
Kwok-kuen Cheung <cheungpat@oursky.com>
2016-11-25 14:34:56 +0800

    Separate SKYContainer and SKYChatExtension

---
+++ SKYKitChat/Classes/SKYKitChat.h
@@ -0,0 +1,19 @@
+//
+//  SKYKitChat.h
+//  Pods
+//
+//  Created by atwork on 25/11/2016.
+//
+//
+
+#import "SKYChatExtension.h"
+#import "SKYContainer+Chat.h"
+
+// record classes
+#import "SKYChatRecord.h"
+#import "SKYChatUser.h"
+#import "SKYConversation.h"
+#import "SKYLastMessageRead.h"
+#import "SKYMessage.h"
+#import "SKYUserChannel.h"
+#import "SKYUserConversation.h"
