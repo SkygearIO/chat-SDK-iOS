968817c299512e19aaa315b5d5deaad0ae6a9b99
Kwok-kuen Cheung <cheungpat@oursky.com>
2016-11-28 10:33:00 +0800

    Update copyright notice

--- SKYKitChat/Classes/SKYChatExtension.m
+++ SKYKitChat/Classes/SKYChatExtension.m
@@ -1,8 +1,8 @@
 //
-//  SKYContainer+Chat.m
+//  SKYChatExtension.m
 //  SKYKit
 //
-//  Copyright 2015 Oursky Ltd.
+//  Copyright 2016 Oursky Ltd.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+//
 
 #import "SKYChatExtension.h"
 
--- SKYKitChat/Classes/SKYContainer+Chat.m
+++ SKYKitChat/Classes/SKYContainer+Chat.m
@@ -2,7 +2,7 @@
 //  SKYContainer+Chat.m
 //  SKYKit
 //
-//  Copyright 2015 Oursky Ltd.
+//  Copyright 2016 Oursky Ltd.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+//
 
 #import "SKYContainer+Chat.h"
 
