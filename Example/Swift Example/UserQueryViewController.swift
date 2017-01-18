eba5791c83fe20d9f07b192ee2c7b65513c75bc6
Ben Lei <benlei@oursky.com>
2017-01-18 17:24:57 +0800

    Update copyright declaration

--- Example/Swift Example/UserQueryViewController.swift
+++ Example/Swift Example/UserQueryViewController.swift
@@ -1,9 +1,20 @@
 //
 //  UserQueryViewController.swift
-//  SKYKitChat
+//  Swift-Example
 //
-//  Created by Ben Lei on 12/1/2017.
-//  Copyright © 2017 Kwok-kuen Cheung. All rights reserved.
+//  Copyright 2016 Oursky Ltd.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import Foundation
