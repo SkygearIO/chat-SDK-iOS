a598be1424106565c1eac7d338919935041a2053
Ben Lei <benlei@oursky.com>
2017-01-23 18:38:45 +0800

    Add util functions to create avatar using initials

--- Example/Swift Example/ConversationListDemoViewController.swift
+++ Example/Swift Example/ConversationListDemoViewController.swift
@@ -24,6 +24,20 @@ class ConversationListDemoViewController: SKYChatConversationListViewController 
         super.viewDidLoad()
 
         self.delegate = self
+        self.dataSource = self
+    }
+}
+
+extension ConversationListDemoViewController: SKYChatConversationListViewControllerDataSource {
+    func listViewController(_ controller: SKYChatConversationListViewController,
+                            avatarImageForConversation conversation: SKYConversation,
+                            atIndexPath indexPath: IndexPath) -> UIImage?
+    {
+        if let title = conversation.title {
+            return UIImage.avatarImage(forInitialsOfName: title)
+        } else {
+            return UIImage.avatarImage(forInitialsOfName: "")
+        }
     }
 }
 
