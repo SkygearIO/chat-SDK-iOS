811e72f203469fcdca1881f0e1b838445cf5cca6
Ho Wa <howa@oursky.com>
2017-09-07 13:10:29 +0800

    .jazzy.yaml added

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 # Update docs.skygear.io
-generate-ios-doc --pwd $PWD/Example --prefix https://github.com/SkygearIO/chat-SDK-iOS/tree/latest
-publish-doc --platform ios --pwd $PWD  --doc-dir $PWD/docs --bucket 'docs.skygear.io' --prefix '/ios/chat/reference' --version 'latest' --distribution-id E31J8XF8IPV2V
+generate-ios-doc --pwd $PWD
+publish-doc --platform ios --pwd $PWD  --doc-dir $PWD/jazzy_docs --bucket 'docs.skygear.io' --prefix '/ios/chat/reference' --version 'latest' --distribution-id E31J8XF8IPV2V
