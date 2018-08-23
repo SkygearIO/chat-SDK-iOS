a0cb5654c46a4c3402fdfe842b7fabeeb0bab350
Ben Lei <benlei@oursky.com>
2018-08-23 19:29:41 +0800

    Ensure an empty line between versions in CHANGELOG

--- scripts/release-commit.sh
+++ scripts/release-commit.sh
@@ -19,7 +19,7 @@ else
 fi
 
 github-release release -u skygeario -r chat-SDK-iOS --draft --tag $SKYGEAR_VERSION --name "$SKYGEAR_VERSION" --description "`cat new-release`"
-cat CHANGELOG.md >> new-release && mv new-release CHANGELOG.md
+echo "" >> new-release && cat CHANGELOG.md >> new-release && mv new-release CHANGELOG.md
 make update-version VERSION=$SKYGEAR_VERSION
 git add CHANGELOG.md SKYKitChat.podspec Example/Podfile.lock
 git commit -m "Update CHANGELOG for $SKYGEAR_VERSION"
